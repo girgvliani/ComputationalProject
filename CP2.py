cbd5ce4db8bf2a6a772bc3d16b443b58bb72a271
NickG <59957371+girgvliani@users.noreply.github.com>
2022-12-15 21:57:30 +0400

    TtoX works

--- CP2.py
+++ CP2.py
@@ -5,14 +5,22 @@ import numpy as np
 This function must create a matrix
 with the sizes suitable for matX
 '''
-def createMatrix(matX):
-    pass
+def createMatrix(matX, k = 2):
+    return fillMatrix(np.ndarray(matX.size), k)
 
+
+'''
+Fills matrix with our desired K 
+to make the transformation similar to Caesar cipher
+'''
+def fillMatrix(matrix, k):
+    for element in range(0,matrix.size):
+        matrix[element] = k
+    return matrix
 
 '''
 gramShmidt algorithms
 '''
-
 def gramShmidt(matV):
     height, width = matV.size()
     matU = np.ndarray(height, width)
@@ -28,10 +36,12 @@ def gramShmidt(matV):
 This function encrypts the data using a Caesar cipher
 '''
 def encryption(input):
-    matX = input.flatten()
-    print(len(matX))
+    matX = input.flatten()#flaten function flattens the whole input
+    print(matX)
     matK = createMatrix(matX)##TODO following code is not correct just for testing
-    print(np.matmul(matX,matK))
+    print(matK)
+    matX[0] = 255
+    print(matX+matK)
 
 
 
