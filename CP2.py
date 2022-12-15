89a76b39e643d0d41e8904113bc4041319a738c8
NickG <59957371+girgvliani@users.noreply.github.com>
2022-12-16 02:10:56 +0400

    mistakes I cant fix

--- CP2.py
+++ CP2.py
@@ -6,20 +6,23 @@ This function must create a matrix
 with the sizes suitable for matX
 '''
 def createMatrix(matX, k = 2):
-    return fillMatrix(np.ndarray(matX.size), k)
+    return fillMatrix(np.ndarray((matX.size,matX.size)), k)
 
 
 '''
 Fills matrix with our desired K 
 to make the transformation similar to Caesar cipher
+this specific K was chosen to show caesar cipher
+essentially this creates an identity matrix nxn size multiplied 
+by k our coeficcient for Caesar Cipher
 '''
 def fillMatrix(matrix, k):
-    for element in range(0,matrix.size):
-        matrix[element] = k
+    for i in range(0,149):
+        matrix[i,i] = k
     return matrix
 
 '''
-gramShmidt algorithms
+GramShmidt algorithms
 '''
 def gramShmidt(matV):
     height, width = matV.size()
@@ -31,20 +34,40 @@ def gramShmidt(matV):
             matU[0:,iter] = matU[0:, iter]-(np.transpose(matU[0:,iter2])*matU[0:,iter])*matU[0:,iter2]
         matU[0:, iter] = matU[0:, iter]/np.norm(matU[0:, iter])
 
+def TtoX(input):
+    matrix = np.ndarray(len(input))
+    if input.type == str:
+        for iter in range(0,input):
+            matrix[iter] = input[iter]
+    return matrix
+
+def XtoY(matX):
+    chunk = 150
+    matY = []
+    iter = 1
+    while iter*chunk*chunk < matX.size:
+        matK = createMatrix(matX[(iter-1)*chunk*chunk:iter*chunk*chunk])
+        matY.append(np.matmul(matX[(iter-1)*chunk*chunk:iter*chunk*chunk],matK))
+        print(matX.size - iter*chunk*chunk)
+        iter += 1
+    if iter*chunk*chunk > matX.size:
+        matK = createMatrix(matX[(iter-1)*chunk*chunk:])
+        matY.append(np.matmul(matX[(iter-1)*chunk:],matK))
+
+    return matY
+
 
 '''
 This function encrypts the data using a Caesar cipher
 '''
 def encryption(input):
-    matX = input.flatten()#flaten function flattens the whole input
+    matX = TtoX(input)#fllaten function flattens the whole input
     print(matX)
-    matK = createMatrix(matX)##TODO following code is not correct just for testing
-    print(matK)
-    print(matX+matK)
+    print(XtoY(matX))
 
 
 
 if __name__ == '__main__':
-    input = cv2.imread("new_img.jpg")
+    input = "image"#cv2.imread("new_img.jpg")
     encryption(input)
 
