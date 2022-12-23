452cda149799050b1deb7dc651452fd4aee919a8
NickG <59957371+girgvliani@users.noreply.github.com>
2022-12-23 18:55:09 +0400

    Delete readinginfofromhtml.py

--- readinginfofromhtml.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-scrapedData = pd.read_csv("https://football-data.co.uk/mmz4281/2223/E0.csv")
-
-scrapedData.rename(columns={"FTHG":"Home_Goals", "FTAG": "Away_Goals"}, inplace=True)
-
-
-print(int("010101111000" ,2))
